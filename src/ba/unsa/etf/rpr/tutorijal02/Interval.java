625f383cba098dbfc81a2a8083ad9b997dfa9fa8
Adna <asestic1@etf.unsa.ba>
2020-11-06 16:57:25 +0100

    Initial commit

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,6 +1,95 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
-public class Interval {
+import java.util.Objects;
 
+public class Interval {
+    private double pocetnaTacka;
+    private double krajnjaTacka;
+    private boolean pocetnaPripadaIntervalu;
+    private boolean krajnjaPripadaIntervalu;
+
+    public Interval(double pocetnaTacka, double krajnjaTacka, boolean pocetnaPripadaIntervalu, boolean krajnjaPripadaIntervalu)
+            throws IllegalArgumentException {
+        if(krajnjaTacka < pocetnaTacka) throw new IllegalArgumentException("Nevalidan interval");
+
+        this.pocetnaTacka = pocetnaTacka;
+        this.krajnjaTacka = krajnjaTacka;
+        this.pocetnaPripadaIntervalu = pocetnaPripadaIntervalu;
+        this.krajnjaPripadaIntervalu = krajnjaPripadaIntervalu;
+    }
+
+    //izuzetak konstruktor
+    public Interval() {
+        pocetnaTacka = 0;
+        krajnjaTacka = 0;
+        pocetnaPripadaIntervalu = false;
+        krajnjaPripadaIntervalu = false;
+    }
+
+    public boolean isNull() {
+        if(pocetnaTacka == 0 && krajnjaTacka == 0 && !pocetnaPripadaIntervalu && !krajnjaPripadaIntervalu) return true;
+        return false;
+    }
+
+    public boolean isIn(double tacka) {
+        if((tacka > pocetnaTacka && tacka < krajnjaTacka) || (tacka == pocetnaTacka && pocetnaPripadaIntervalu) ||
+                (tacka == krajnjaTacka && krajnjaPripadaIntervalu)) return true;
+        return false;
+    }
+
+    public Interval intersect(Interval drugi) {
+        //poredimo pocetne i krajnje tacke i onda uzimamo manju krajnju i vecu pocetnu
+        //treba ukljuciti i provjeru da li se pocetne/krajnje tacke nalaze unutar intervala
+        double pocetnaTacka = 0;
+        double krajnjaTacka = 0;
+        boolean pocetnaPripadaIntervalu = false;
+        boolean krajnjaPripadaIntervalu = false;
+
+        if(this.pocetnaTacka >= drugi.pocetnaTacka) pocetnaTacka = this.pocetnaTacka;
+        else pocetnaTacka = drugi.pocetnaTacka;
+
+        if(this.krajnjaTacka <= drugi.krajnjaTacka) krajnjaTacka = this.krajnjaTacka;
+        else krajnjaTacka = drugi.krajnjaTacka;
+
+        if((this.pocetnaTacka == pocetnaTacka && this.pocetnaPripadaIntervalu) || (drugi.pocetnaTacka == pocetnaTacka && drugi.pocetnaPripadaIntervalu)) {
+            pocetnaPripadaIntervalu = true;
+        }
+
+        if((this.krajnjaTacka == krajnjaTacka && this.krajnjaPripadaIntervalu) || (drugi.krajnjaTacka == krajnjaTacka && drugi.krajnjaPripadaIntervalu)) {
+            krajnjaPripadaIntervalu = true;
+        }
+
+        return new Interval(pocetnaTacka, krajnjaTacka, pocetnaPripadaIntervalu, krajnjaPripadaIntervalu);
+
+    }
+
+    public static Interval intersect(Interval prvi, Interval drugi) {
+        return prvi.intersect(drugi);
+    }
+
+    @Override
+    public String toString() {
+
+        if(this.isNull()) return "()";
+
+        String vracamo = "";
+        if(pocetnaPripadaIntervalu) vracamo = vracamo + "[";
+        else vracamo = vracamo + "(";
+        vracamo = vracamo + String.valueOf(pocetnaTacka) + "," + String.valueOf(krajnjaTacka);
+        if(krajnjaPripadaIntervalu) vracamo = vracamo + "]";
+        else vracamo = vracamo + ")";
+        return vracamo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Interval interval = (Interval) o;
+        return Double.compare(interval.pocetnaTacka, pocetnaTacka) == 0 &&
+                Double.compare(interval.krajnjaTacka, krajnjaTacka) == 0 &&
+                pocetnaPripadaIntervalu == interval.pocetnaPripadaIntervalu &&
+                krajnjaPripadaIntervalu == interval.krajnjaPripadaIntervalu;
+    }
 
 }
